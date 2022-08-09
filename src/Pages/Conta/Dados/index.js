75c57b8df6efeffe39fd064df0230f928531648e
Bruno Milano <brn.milano@hotmail.com>
2022-08-09 12:44:39 -0300

    Criação dos componentes Dados e MoreInfos (conta)

---
+++ src/Pages/Conta/Dados/index.js
@@ -0,0 +1,87 @@
+import Box from "@mui/material/Box";
+import styles from "./styles.module.scss";
+import Text from "../../../Components/Text";
+import { useState } from "react";
+import { Skeleton } from "@mui/material";
+import moment from "moment";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+
+export default function Dados() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [dados, setDados] = useState([]);
+
+  setTimeout(() => {
+    setDados([
+      {
+        info: "Nome:",
+        content: "Bruno Milano",
+        type: "string",
+      },
+      {
+        info: "CPF:",
+        content: "000.000.000-00",
+        type: "string",
+      },
+      {
+        info: "Data de nascimento:",
+        content: "1996-07-26",
+        type: "date",
+      },
+      {
+        info: "RG:",
+        content: "0.000.000",
+        type: "string",
+      },
+      {
+        info: "Email:",
+        content: "brn.milano@gmail.com",
+        type: "string",
+      },
+      {
+        info: "Telefone:",
+        content: "(61) 9 8342-6022",
+        type: "string",
+      },
+    ]);
+    setIsLoading(false);
+  }, 1500);
+
+  return (
+    <Box className={styles.container}>
+      {isLoading ? (
+        <>
+          <Skeleton variant="text" width={200} height={20} />
+          <Skeleton variant="text" width={200} height={20} />
+          <Skeleton variant="text" width={200} height={20} />
+          <Skeleton variant="text" width={200} height={20} />
+          <Skeleton variant="text" width={200} height={20} />
+          <Skeleton variant="text" width={200} height={20} />
+        </>
+      ) : (
+        <>
+          {dados.map((item, index) => {
+            return (
+              <Box className={styles.infos}>
+                <Text fontWeight={700} fontSize={14}>
+                  {item.info}
+                </Text>
+
+                <Text fontSize={14}>
+                  {item.type === "date"
+                    ? moment(item.content).format("DD/MM/YYYY")
+                    : item.content}
+                </Text>
+              </Box>
+            );
+          })}
+        </>
+      )}
+
+      <a href="http://localhost:3000/" rel="noopener noreferrer">
+        <Box>
+          <EditOutlinedIcon />
+        </Box>
+      </a>
+    </Box>
+  );
+}
---
+++ src/Pages/Conta/MoreInfos/index.js
@@ -0,0 +1,91 @@
+import Box from "@mui/material/Box";
+import styles from "./styles.module.scss";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import Heading from "../../../Components/Heading";
+import Text from "../../../Components/Text";
+
+export default function MoreInfos() {
+  return (
+    <Box className={styles.container}>
+      <Box className={styles.moreInfos}>
+        <Box>
+          <PersonOutlineOutlinedIcon />
+          <Heading fontSize={16}>Conta</Heading>
+        </Box>
+
+        <Box className={styles.contentWrapper}>
+          <a href="#espera" rel="noopener noreferrer">
+            <Text fontWeight={300} fontSize={16} color="#000000">
+              Editar endereço
+            </Text>
+
+            <Box className={styles.arrow}>
+              <KeyboardArrowRightOutlinedIcon />
+            </Box>
+          </a>
+
+          <a href="http://localhost:3000/" rel="noopener noreferrer">
+            <Text fontWeight={300} fontSize={16} color="#000000">
+              Dados Bancários
+            </Text>
+
+            <Box className={styles.arrow}>
+              <KeyboardArrowRightOutlinedIcon />
+            </Box>
+          </a>
+        </Box>
+      </Box>
+
+      <Box className={styles.moreInfos}>
+        <Box>
+          <AdminPanelSettingsOutlinedIcon />
+          <Heading fontSize={16}>Segurança</Heading>
+        </Box>
+
+        <Box className={styles.contentWrapper}>
+          <a href="http://localhost:3000/" rel="noopener noreferrer">
+            <Text fontWeight={300} fontSize={16} color="#000000">
+              Alterar senha
+            </Text>
+
+            <Box className={styles.arrow}>
+              <KeyboardArrowRightOutlinedIcon />
+            </Box>
+          </a>
+        </Box>
+      </Box>
+
+      <Box className={styles.moreInfos}>
+        <Box>
+          <InfoOutlinedIcon />
+          <Heading fontSize={16}>Informações</Heading>
+        </Box>
+
+        <Box className={styles.contentWrapper}>
+          <a href="http://localhost:3000/" rel="noopener noreferrer">
+            <Text fontWeight={300} fontSize={16} color="#000000">
+              Política de privacidade
+            </Text>
+
+            <Box className={styles.arrow}>
+              <KeyboardArrowRightOutlinedIcon />
+            </Box>
+          </a>
+
+          <a href="http://localhost:3000/" rel="noopener noreferrer">
+            <Text fontWeight={300} fontSize={16} color="#000000">
+              Termos de uso
+            </Text>
+
+            <Box className={styles.arrow}>
+              <KeyboardArrowRightOutlinedIcon />
+            </Box>
+          </a>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
