16d1e23fd1415835f40ceee56bd18c65437a9100
Bruno Milano <brn.milano@hotmail.com>
2022-08-10 22:08:25 -0300

    Ajustes no componente SideBar

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -7,6 +7,7 @@ import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import Text from "../Text";
 import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 export default function SideBar() {
   return (
@@ -20,43 +21,43 @@ export default function SideBar() {
           <Box className={styles.menuItems}>
             <HomeOutlinedIcon />
 
-            <a href="/home" rel="noopener noreferrer">
+            <Link to="/home" rel="noopener noreferrer">
               <Text fontWeight={500} fontSize={14}>
                 Home
               </Text>
-            </a>         
+            </Link>
           </Box>
 
           <Box className={styles.menuItems}>
             <LibraryBooksOutlinedIcon />
-            <a href="/contratos" rel="noopener noreferrer">
+            <Link to="/contratos" rel="noopener noreferrer">
               <Text fontWeight={500} fontSize={14}>
                 Contratos
               </Text>
-            </a>
+            </Link>
           </Box>
 
           <Box className={styles.menuItems}>
             <HelpOutlineOutlinedIcon />
-            <a href="/duvidas" rel="noopener noreferrer">
+            <Link to="/duvidas" rel="noopener noreferrer">
               <Text fontWeight={500} fontSize={14}>
                 Dúvidas
               </Text>
-            </a>
+            </Link>
           </Box>
 
           <Box className={styles.menuItems}>
             <PersonOutlineOutlinedIcon />
-            <a href="/conta" rel="noopener noreferrer">
+            <Link to="/conta" rel="noopener noreferrer">
               <Text fontWeight={500} fontSize={14}>
                 Conta
               </Text>
-            </a>
+            </Link>
           </Box>
         </Box>
 
         <Box className={styles.buttonLogout}>
-          <Button href="#">Sair</Button>
+          <Button href="/">Sair</Button>
         </Box>
       </Box>
     </Box>
